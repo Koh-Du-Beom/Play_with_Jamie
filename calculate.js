7e992c3a13d770305d3d4b3081a505fb4480fc50
zs <zs@zsui-MacBookPro.local>
2024-01-30 15:44:04 +0900

    sdsdfsdf

--- calculate.js
+++ calculate.js
@@ -1,7 +1,7 @@
 function test1() {
   console.log("test by Koh-Du-Beom");
-	console.log('hi new code');
-  console.log("edited by koh");
+  console.log("edited by kimsdfimsd");
+  console.log("hi i'm code");
 }
 
 function test2() {
